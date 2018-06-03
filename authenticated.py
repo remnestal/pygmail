43b51eeabdc699af1680f72bfef1f53cd8f16cf5
Albin Remnestål <albin.remnestal@gmail.com>
2018-06-03 11:22:33 +0200

    doc: make the docstrings more coherent

--- authenticated.py
+++ authenticated.py
@@ -6,32 +6,32 @@ from oauth2client.file import Storage
 
 
 class Session(object):
-    """ Handler class for making authenticated calls to the Gmail API """
+    """Handler class for making authenticated calls to the Gmail API"""
 
     def __init__(self, storage=None, secret=None):
-        """ Set up the authenticated session with the Gmail API """
+        """Set up the authenticated session with the Gmail API"""
         self.token = Token(storage, secret)
         self.service = self.start_service()
 
     def start_service(self):
-        """ Initialize the API connection """
+        """Initialize the API connection"""
         http = self.token().authorize(httplib2.Http())
         return discovery.build('gmail', 'v1', http=http)
 
     def __call__(self):
-        """ Return the Gmail API handle """
+        """Return the Gmail API handle"""
         return self.service
 
 
 class Token(Storage):
-    """ Class for storing and managing an Oauth2 refresh token """
+    """Class for storing and managing an Oauth2 refresh token"""
 
     oauth_scope = 'https://www.googleapis.com/auth/gmail.compose'
     default_storage = 'storage.json'
     default_secret = 'client_secret.json'
 
     def __init__(self, storage=None, secret=None, invalidate=False):
-        """ Read a stored Oauth2 token or generate a new one """
+        """Read a stored Oauth2 token or generate a new one"""
 
         self.storage = storage or self.default_storage
         self.secret = secret or self.default_secret
@@ -46,14 +46,14 @@ class Token(Storage):
                 self.credentials = stored_token
 
     def __call__(self):
-        """ Return the OAuth2Credentials associated with this token """
+        """Return the OAuth2Credentials associated with this token"""
         return self.credentials
 
     def refresh_token(self):
-        """ Request a new set of credentials via Google's auth-flow
+        """Request a new set of credentials via Google's auth-flow
 
-            Raises:
-                FileNotFoundError: if client-secret file does not exist
+        Raises:
+            FileNotFoundError: if client-secret file does not exist
         """
 
         # check that the specified client secret file exists
