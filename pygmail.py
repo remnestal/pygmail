213f3286eb9e3947ba36717df1dfbd0cbeb1503b
Albin Remnestål <albin.remnestal@gmail.com>
2018-06-04 22:44:19 +0200

    feat: add standalone send functionality

--- pygmail.py
+++ pygmail.py
@@ -19,6 +19,11 @@ class Pygmail(object):
                  invalidate=False):
         self.api = Session(storage, secret, oauth_scope, invalidate)
 
+    def authorized_address(self):
+        """Return the email-address of the user authorized in this session"""
+        profile = self.api().users().getProfile(userId="me").execute()
+        return profile['emailAddress']
+
     def send(self, mail:Mail):
         """Send the passed mail object via the Gmail API
 
@@ -31,22 +36,38 @@ class Pygmail(object):
 def main():
     """Run pygmail as a standalone module"""
     args = parse_arguments()
-
     if args.auth:
         # run authorization flow anew
         pyg = Pygmail(args.token, args.credentials, invalidate=True)
-
-    if args.auth:
-        pyg = Pygmail(args.token, args.credentials, invalidate=True)
+    elif args.send:
+        pyg = Pygmail(args.token, args.credentials)
+        mail = Mail(args.recipients,
+                    pyg.authorized_address(),
+                    subject=args.subject,
+                    body=args.body)
+        print(pyg.send(mail))
 
 def parse_arguments():
     """Parses input arguments and options"""
     parser = argparse.ArgumentParser()
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--auth", action="store_true")
-    parser.add_argument("-c", "--credentials", help="client-secret file")
-    parser.add_argument("-t", "--token", help="token-storage file")
+    group.add_argument('--auth', action='store_true',
+                       help='authorize pygmail to send emails from your account.')
+    group.add_argument('--send', action='store_true',
+                       help='send an email through pygmail.')
+    parser.add_argument('-c', '--credentials',
+                        help='location of client-secret file')
+    parser.add_argument('-t', '--token',
+                        help='location of token-storage file')
+
+    # email message configuration
+    parser.add_argument('-s', '--subject',
+                        help='subject line of the email')
+    parser.add_argument('-b', '--body',
+                        help='text body of the email')
+    parser.add_argument('-r', '--recipients',
+                        help='space-separated list of recipients\' email addresses')
 
     args = parser.parse_args()
     sys.argv = [sys.argv[0]]
